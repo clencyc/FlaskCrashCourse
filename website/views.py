15f9018174c5fe2934faecdb7a8fef1a7b2382ee
clencyc <clency2023@gmail.com>
2024-09-13 01:28:43 +0300

    new commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # define the blueprint
 # Blueprint is a way to organize a group of related views and other code
@@ -7,4 +7,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
