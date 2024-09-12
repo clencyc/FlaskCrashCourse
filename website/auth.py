e05b31de82db1eacf31b002341cfd496124c926d
clencyc <clency2023@gmail.com>
2024-09-13 00:46:26 +0300

    gitignore

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+# define the blueprint
+# Blueprint is a way to organize a group of related views and other code
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
