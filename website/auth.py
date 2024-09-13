9720b1a5a6d70cbb1126c8331bd50d6f8c6a0bea
clencyc <clency2023@gmail.com>
2024-09-13 22:44:58 +0300

    Added database

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 # define the blueprint
 # Blueprint is a way to organize a group of related views and other code
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data) 
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) > 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            flash('Account created!', category='success')
+            # add user to database
+            
+    return render_template("sign_up.html")
